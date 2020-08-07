437f3c68175699b94ddba08b8aa710d1ccce03b8
yufi <yufi.gitlab@gmail.com>
2020-08-08 05:38:37 +0700

    update

--- coba crud/database.php
+++ coba crud/database.php
@@ -7,10 +7,19 @@ class database{
   var $db = "user";
 
   function __construck(){
-    mysqli_connect($this->host, $this->uname, $this->pass);
+    $conn = mysqli_connect($this->host, $this->uname, $this->pass);
     mysqli_select_db($this->db);
   }
+
+  function tampil_data(){
+   $data = mysqli_query($conn, "SELECT * FROM user");
+   while ($d = mysqli_fetch_array($data)){
+     $hasil[] = $d;
+   }
+    return $hasil;
+  }
 }
+
 
 
 ?>
--- coba crud/tampil.php
+++ coba crud/tampil.php
@@ -22,7 +22,25 @@ $db = new Database();
     <th>Nama</th>
     <th>Alamat</th>
     <th>Usia</th>
+    <th>Aksi</th>
   </tr>
+  <?php 
+   $no = 1;
+   foreach($db->tampil_data() as $data) :
+  ?>
+
+  <tr>
+    <td><?php echo $no++; ?></td>
+    <td><?php echo $data['nama']; ?></td>
+    <td><?php echo $data['alamat']; ?></td>
+    <td><?php echo $data['usia'] ?></td>
+    <td>
+      <a href="edit.php?id=<php echo $data['id']; ?>&aksi=edit">Edit</a> |
+      <a href="delete.php?id=<php echo $data['id']; ?>&aksi=delete">Delete</a>
+    </td>
+  </tr>
+
+   <?php endforeach; ?>
 
 </table>
 
